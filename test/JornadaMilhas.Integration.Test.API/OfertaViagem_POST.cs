6d0a847991259bc4e452bd7c9ca80796d9366483
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-07-29 22:23:21 -0300

    @GustavoCarvalho25 - Compartilhando contexto via Fixture

--- test/JornadaMilhas.Integration.Test.API/OfertaViagem_POST.cs
+++ test/JornadaMilhas.Integration.Test.API/OfertaViagem_POST.cs
@@ -10,14 +10,20 @@ using System.Threading.Tasks;
 
 namespace JornadaMilhas.Integration.Test.API
 {
-    public class OfertaViagem_POST
+    public class OfertaViagem_POST:IClassFixture<JornadaMilhasWebApplicationFactory>
     {
+        private readonly JornadaMilhasWebApplicationFactory _app;
+
+        public OfertaViagem_POST(JornadaMilhasWebApplicationFactory app)
+        {
+            _app = app;
+        }
+
         [Fact]
         public async Task Cadastra_OfertaViagem()
         {
             //Arrange
-            var app = new JornadaMilhasWebApplicationFactory();
-            using var client = await app.GetClientWithAccessTokenAsync();
+            using var client = await _app.GetClientWithAccessTokenAsync();
 
             var ofertaViagem = new OfertaViagem()
             {
@@ -31,6 +37,26 @@ namespace JornadaMilhas.Integration.Test.API
 
             //Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode) ;
+        }
+
+        [Fact]
+        public async Task Cadastra_OfertaViagem_Sem_Autorizacao()
+        {
+            //Arrange
+            using var client = _app.CreateClient();
+
+            var ofertaViagem = new OfertaViagem()
+            {
+                Preco = 100,
+                Rota = new Rota("Origem", "Destino"),
+                Periodo = new Periodo(DateTime.Parse("2024-03-03"), DateTime.Parse("2024-03-06"))
+            };
+
+            //Act
+            var response = await client.PostAsJsonAsync("ofertas-viagem", ofertaViagem);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
         }
     }
 }
