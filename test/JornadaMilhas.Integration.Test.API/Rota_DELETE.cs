1d0d3653fe7f66a39119bc4e0c5673720236c2d8
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-07-30 23:55:05 -0300

    @GustavoCarvalho25 - Adicionando classe de teste DELETE para Rotas

---
+++ test/JornadaMilhas.Integration.Test.API/Rota_DELETE.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JornadaMilhas.Integration.Test.API
+{
+    public class Rota_DELETE : IClassFixture<JornadaMilhasWebApplicationFactory>
+    {
+        private readonly JornadaMilhasWebApplicationFactory _app;
+
+        public Rota_DELETE(JornadaMilhasWebApplicationFactory app)
+        {
+            _app = app;
+        }
+    }
+}
