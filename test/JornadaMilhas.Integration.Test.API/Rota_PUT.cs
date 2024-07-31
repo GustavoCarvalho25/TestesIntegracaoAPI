d877e76617f70b244b20b0f01d96c830185250ec
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-07-30 23:59:39 -0300

    @GustavoCarvalho25 - Adicionando teste de atualização

--- test/JornadaMilhas.Integration.Test.API/Rota_PUT.cs
+++ test/JornadaMilhas.Integration.Test.API/Rota_PUT.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using JornadaMilhas.Dominio.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +16,39 @@ namespace JornadaMilhas.Integration.Test.API
         public Rota_PUT(JornadaMilhasWebApplicationFactory app)
         {
             _app = app;
+        }
+
+        [Fact]
+        public async Task Atualiza_Rota()
+        {
+            //arrange
+            var rotaExistente = _app.Context.Rota.FirstOrDefault();
+
+            if (rotaExistente is null)
+            {
+                rotaExistente = new Rota()
+                {
+                    Origem = "Origem",
+                    Destino = "Destino"
+                };
+                _app.Context.Add(rotaExistente);
+                _app.Context.SaveChanges();
+            }
+
+            using var client = await _app.GetClientWithAccessTokenAsync();
+            var rota = new
+            {
+                Id = rotaExistente.Id,
+                Origem = "Origem",
+                Destino = "Destino"
+            };
+
+            //act
+            var response = await client.PutAsJsonAsync($"/rota-viagem/{rotaExistente.Id}", rota);
+
+            //assert
+            Assert.NotNull(response);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
     }
 }
