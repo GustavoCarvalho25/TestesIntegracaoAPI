c2352d6b557283e106d8aa5f03e2c78ee2a5649d
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-07-30 23:56:06 -0300

    @GustavoCarvalho25 - Adicionando classe de teste PUT para Rotas

---
+++ test/JornadaMilhas.Integration.Test.API/Rota_PUT.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JornadaMilhas.Integration.Test.API
+{
+    public class Rota_PUT : IClassFixture<JornadaMilhasWebApplicationFactory>
+    {
+        private readonly JornadaMilhasWebApplicationFactory _app;
+
+        public Rota_PUT(JornadaMilhasWebApplicationFactory app)
+        {
+            _app = app;
+        }
+    }
+}
