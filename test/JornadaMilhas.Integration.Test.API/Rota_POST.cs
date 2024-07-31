63a9f618ae3ddc000f636b542b0ed07252bc02b6
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-07-30 23:58:37 -0300

    @GustavoCarvalho25 - Adicionando teste CriaRota para verificação de endpoint POST

--- test/JornadaMilhas.Integration.Test.API/Rota_POST.cs
+++ test/JornadaMilhas.Integration.Test.API/Rota_POST.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +15,33 @@ namespace JornadaMilhas.Integration.Test.API
         public Rota_POST(JornadaMilhasWebApplicationFactory app)
         {
             _app = app;
+        }
+
+        [Fact]
+        public async Task Cadastra_Rota()
+        {
+            //arrange
+            var rotaExistente = _app.Context.Rota.FirstOrDefault();
+
+            if (rotaExistente is not null)
+            {
+                _app.Context.Remove(rotaExistente);
+                _app.Context.SaveChanges();
+            }
+
+            using var client = await _app.GetClientWithAccessTokenAsync();
+            var rota = new
+            {
+                Origem = "Origem",
+                Destino = "Destino"
+            };
+
+            //act
+            var response = await client.PostAsJsonAsync($"/rota-viagem/", rota);
+
+            //assert
+            Assert.NotNull(response);
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
         }
     }
 }
