4ec8d5e958a205cb359ee7e35e1c0f9e9ddc00b9
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-07-30 23:55:51 -0300

    @GustavoCarvalho25 - Adicionando classe de teste POST para Rotas

---
+++ test/JornadaMilhas.Integration.Test.API/Rota_POST.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JornadaMilhas.Integration.Test.API
+{
+    public class Rota_POST : IClassFixture<JornadaMilhasWebApplicationFactory>
+    {
+        private readonly JornadaMilhasWebApplicationFactory _app;
+
+        public Rota_POST(JornadaMilhasWebApplicationFactory app)
+        {
+            _app = app;
+        }
+    }
+}
