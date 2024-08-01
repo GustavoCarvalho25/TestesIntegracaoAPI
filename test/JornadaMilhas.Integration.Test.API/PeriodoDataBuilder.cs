7ec41916f6fde9b49073ac373a6e5f15aefce82f
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-07-31 23:59:25 -0300

    @GustavoCarvalho25 - Criando construtor

--- test/JornadaMilhas.Integration.Test.API/PeriodoDataBuilder.cs
+++ test/JornadaMilhas.Integration.Test.API/PeriodoDataBuilder.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Bogus;
+using JornadaMilhas.Dominio.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,19 @@ using System.Threading.Tasks;
 
 namespace JornadaMilhas.Integration.Test.API
 {
-    internal class PeriodoDataBuilder
+    public class PeriodoDataBuilder : Faker<Periodo>
     {
+        public DateTime? DataInicio { get; set; }
+        public DateTime? DataFim { get; set; }
+
+        public PeriodoDataBuilder()
+        {
+            CustomInstantiator(f =>
+            {
+                DateTime dataInicio = DataInicio ?? f.Date.Soon();
+                DateTime dataFim = DataFim ?? dataInicio.AddDays(f.Random.Int(1, 10));
+                return new Periodo(dataInicio, dataFim);
+            });
+        }
     }
 }
