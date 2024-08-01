5e1132370291cbb288990e04dd71a805a9ad7a55
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-01 17:47:34 -0300

    @GustavoCarvalho25 - Atualizando classe RotaDataBuilder

--- test/JornadaMilhas.Integration.Test.API/RotaDataBuilder.cs
+++ test/JornadaMilhas.Integration.Test.API/RotaDataBuilder.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Bogus;
+using JornadaMilhas.Dominio.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,21 @@ using System.Threading.Tasks;
 
 namespace JornadaMilhas.Integration.Test.API
 {
-    internal class RotaDataBuilder
+    public class RotaDataBuilder : Faker<Rota>
     {
+        public string? Origem { get; set; }
+        public string? Destino { get; set; }
+
+        public RotaDataBuilder()
+        {
+            CustomInstantiator(f =>
+            {
+                string origem = Origem ?? f.Lorem.Sentence(2);
+                string destino = Destino ?? f.Lorem.Sentence(2);
+                return new Rota(origem, destino);
+            });
+        }
+
+        public Rota Build() => Generate();
     }
 }
