a1abb8c0828440a94ea9df7a25729e000f832de2
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-07-31 23:49:02 -0300

    @GustavoCarvalho25 - Criando DataBuilder para geração de OfertaViagens

---
+++ test/JornadaMilhas.Integration.Test.API/OfertaViagemDataBuilder.cs
@@ -0,0 +1,23 @@
+﻿using Bogus;
+using JornadaMilhas.Dominio.Entidades;
+using JornadaMilhas.Dominio.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JornadaMilhas.Integration.Test.API
+{
+    public class OfertaViagemDataBuilder : Faker<OfertaViagem>
+    {
+        public Rota? Rota { get; set; }
+        public Periodo? Periodo { get; set; }
+        public double PrecoMinimo { get; set; } = 1;
+        public double PrecoMaximo { get; set; } = 100.0;
+
+        public OfertaViagemDataBuilder()
+        {
+        }
+    }
+}
